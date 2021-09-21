6bbf686bffb718629ea5f26c6e6b170d8be484cf
IAS-MDMA\IAAdmin <ia.space@zhdk.ch>
2021-09-21 16:22:30 +0200

    updated launcher

--- windows/maya.ps1
+++ windows/maya.ps1
@@ -1,26 +1,22 @@
 
 $BASE="$pwd\..\..\"
 
-$ECO_ENV="$BASE\Ecosystem-Env\"
-$PYTHONPATH=$BASE
-
-$Env:Path+=";$ECO_ENV"
-$Env:Path+=";$BASE"
+$Env:ECO_ENV="$BASE\Ecosystem-Env"
+$Env:PYTHONPATH="$BASE\Ecosystem"
 
 # echo $args[0]
 
 If ($args[0] -eq "2019") {
-    $TOOLS="base,maya2019"
+    $Env:TOOLS="base,maya2019"
 }
 elseif ($args[0] -eq "2020"){
-    $TOOLS="base,maya2020"
+    $Env:TOOLS="base,maya2020"
 }
 elseif ($args[0] -eq "2022"){
-    $TOOLS="base,maya2022"
+    $Env:TOOLS="base,maya2022"
 }
 
-$Env:Path+=";$TOOLS"
 
-python $BASE\Ecosystem\ecosystem\main.py -t $TOOLS -s > "C:\Users\Public\tmp\ecosystem.env"
+python $Env:PYTHONPATH\ecosystem\main.py -t $Env:TOOLS -s > "C:\Users\Public\tmp\ecosystem.env"
 
 set "C:\Users\Public\tmp\ecosystem.env"
