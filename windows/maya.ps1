d5b05c47c4ab48e900086231c46fc6b3d000c92a
Patxi <procedural.godart@gmail.com>
2022-02-01 01:39:21 +0100

    maya finally dinamically starting up

--- windows/maya.ps1
+++ windows/maya.ps1
@@ -1,23 +1,28 @@
 
-$BASE="$pwd\..\..\"
-
-$Env:ECO_ENV="$BASE\Ecosystem-Env"
-$Env:PYTHONPATH="$BASE\Ecosystem"
+# $BASE="$HOME\devel\MyWorkEnv"
+# if using anaconda. Copy ecosystem to C:\tools\Anaconda3\envs\vfxpipeline\Lib\site-packages
+# $env:ECO_ENV="$HOME\devel\MyWorkEnv\ecosystem-env"
+$env:ECO_ENV="$HOME\devel\Ecosystem-Env"
+$env:PYTHONPATH="$HOME\devel\MyWorkEnv\ecosystem"
 
 # echo $args[0]
 
-If ($args[0] -eq "2019") {
-    $Env:TOOLS="base,maya2019"
-}
-elseif ($args[0] -eq "2020"){
-    $Env:TOOLS="base,maya2020"
-}
-elseif ($args[0] -eq "2022"){
-    $Env:TOOLS="base,maya2022"
-}
+# If ($args[0] -eq "2019") {
+#     $Env:TOOLS="base,maya2019"
+# }
+# elseif ($args[0] -eq "2020"){
+#     $Env:TOOLS="base,maya2020"
+# }
+# elseif ($args[0] -eq "2022"){
+#     $Env:TOOLS="base,maya2022"
+# }
 
+$ARGLIST = $args[0].Split(" ")
 
-python $Env:PYTHONPATH\ecosystem\main.py -t $Env:TOOLS -r maya 
+$env:TOOLS = $ARGLIST[0] + "," + $ARGLIST[1]
+echo $env:TOOLS
+
+python $Env:PYTHONPATH\main.py -t $env:TOOLS -r maya 
 #-s > "C:\Users\Public\tmp\ecosystem.env"
 
 # set "C:\Users\Public\tmp\ecosystem.env"
