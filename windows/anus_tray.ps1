8f9589d655b6bc746ed2e8ee4e5325340ccd9a5e
Patxi <procedural.godart@gmail.com>
2022-06-13 15:47:01 +0200

    path to launchers

--- windows/anus_tray.ps1
+++ windows/anus_tray.ps1
@@ -4,19 +4,19 @@ $env:ECO_ENV="$env:DEVEL_BASE\MyWorkEnv\ecosystem-env"
 $env:PYTHONPATH="$env:DEVEL_BASE\MyWorkEnv\"
 
 # LAUNCHERS
-$env:LAUNCHER_FILES="$env:DEVEL_BASE/launcher.files"
-$env:LAUNCHERS="$env:DEVEL_BASE/launchers/unix"
+$env:LAUNCHER_FILES="$env:DEVEL_BASE\launcher.files"
+$env:LAUNCHERS="$env:DEVEL_BASE\launchers\windows"
 
 # APPS
-$env:ANUS_TRAY="$env:DEVEL_BASE/my.tray/anus_tray"
+$env:ANUS_TRAY="$env:DEVEL_BASE\my.tray\anus_tray"
 
-$env:ANUS_PROJECT_MANAGER="$env:ANUS_TRAY/project_manager"
-$env:ANUS_PROJECT_MEMORY="$HOME/.anus/project_history.txt"
+$env:ANUS_PROJECT_MANAGER="$env:ANUS_TRAY\project_manager"
+$env:ANUS_PROJECT_MEMORY="$HOME\.anus\project_history.txt"
 
-$env:ANUS_PROJECT_SETUP="$env:DEVEL_BASE/my.project-setup"
+$env:ANUS_PROJECT_SETUP="$env:DEVEL_BASE\my.project-setup"
 
 
 $SCRIPT_PATH = "$env:DEVEL_BASE\my.tray"
 conda activate vfxpipeline
 
-pythonw "${SCRIPT_PATH}\anus_tray\main.py" 
+python "${SCRIPT_PATH}\anus_tray\main.py" 
